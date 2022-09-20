c35f3e0f770dc1fc9d7a0fc499c020f859e8f66e
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-09-19 21:24:23 -0500

    added flask sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -7,20 +7,31 @@ app.config['SECRET_KEY'] = 'bananaboatxyz987'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False 
 debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_KEY ="responses"
 # 370-263-852
 
 @app.route('/')
 def show_survey_info():
     """show home page for survey site"""
+
     title = survey.title
     instructions = survey.instructions
     return render_template('info.html', title = title, instructions= instructions)
 
+@app.route('/start', methods=['POST'])
+def start_survey():
+    """Set session responses to empty list"""
+    session[RESPONSES_KEY] = []
+
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:index>')
 def send_to_next_question(index):
     """shows user correct question form"""
+    responses = session.get(RESPONSES_KEY)
 
+    if responses is None:
+        return redirect('/')
     if (len(responses) == len(survey.questions)):
         return redirect('/complete')
     if (len(responses) != index):
@@ -35,7 +46,10 @@ def handle_questions():
     """handle questions POST request"""
     answer = request.form['answer']
     
+    responses = session[RESPONSES_KEY]
     responses.append(answer)
+    session[RESPONSES_KEY]
+
     flash('answer submitted', 'success')
 
     if(len(responses) == len(survey.questions)):
